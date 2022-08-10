00153dd450520488f46ce8806d8e1bd40d0ee115
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-08-11 04:15:45 +0900

    Refactoring parser

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -1,17 +1,18 @@
-let rec parse_statement_list tokens =
-  match tokens with
-  | Lexer.Semicolon :: rest -> parse_statement_list rest
-  | Lexer.ReturnKeyword :: Lexer.Int n :: rest ->
-      let other_statements, r = parse_statement_list rest in
-      (Ast.Return (Ast.Const n) :: other_statements, r)
-  | Lexer.ReturnKeyword :: rest ->
-      let other_statements, r = parse_statement_list rest in
-      (* TODO: Is ok `return`'s default value 0 *)
-      (Ast.Return (Ast.Const 0) :: other_statements, r)
-  | _ -> ([], tokens)
-
-let parse_statement tokens =
-  let statements, rest = parse_statement_list tokens in
+let parse_statements tokens =
+  (* Split the statements and the rest *)
+  let rec partition sub_tokens =
+    match sub_tokens with
+    | Lexer.Semicolon :: rest -> partition rest
+    | Lexer.ReturnKeyword :: Lexer.Int n :: rest ->
+        let other_statements, sub_rest = partition rest in
+        (Ast.Return (Ast.Const n) :: other_statements, sub_rest)
+    | Lexer.ReturnKeyword :: rest ->
+        let other_statements, sub_rest = partition rest in
+        (* TODO: Is ok `return`'s default value 0 *)
+        (Ast.Return (Ast.Const 0) :: other_statements, sub_rest)
+    | _ -> ([], sub_tokens)
+  in
+  let statements, rest = partition tokens in
   match rest with
   | Lexer.CloseBrace :: [] -> statements
   | _ -> failwith "Parse error. `}` is missing."
@@ -21,7 +22,7 @@ let parse_function_def tokens =
   | Lexer.IntKeyword
     :: Lexer.Id name
     :: Lexer.OpenParen :: Lexer.CloseParen :: Lexer.OpenBrace :: rest ->
-      Ast.Function (Ast.Id name, parse_statement rest)
+      Ast.Function (Ast.Id name, parse_statements rest)
   | _ -> failwith "Parse error in a function." (* TODO: Kind error message *)
 
 let parse tokens = Ast.Program (parse_function_def tokens)
